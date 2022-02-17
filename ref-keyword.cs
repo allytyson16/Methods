6a8a46ad11b16f7d230a94145ed20f24b71892d1
Adelaide <88384474+allytyson16@users.noreply.github.com>
2022-02-17 13:12:58 +0000

    Add files via upload

---
+++ ref-keyword.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ref_Keyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int studentGrade = 75;
+            GiveExtraCredit(ref studentGrade);
+            Console.WriteLine(studentGrade);
+        }
+        public static void GiveExtraCredit(ref int studentGrade)
+        {
+            studentGrade += 3;
+
+        }
+    }
+}
