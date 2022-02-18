b17cce00fbf088ee82198ed35a870049b5c12728
Adelaide <88384474+allytyson16@users.noreply.github.com>
2022-02-18 18:40:34 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace params_keyword
+{
+    class Program
+    {
+//params allows to enter any amount of characters/argument
+        static void Main(string[] args)
+        {
+            int[] myArray = { 3, 4, 5, 6, 7, 8, 9};
+            PrintArray(myArray);
+            PrintArray(3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 34, 23, 34);
+        }
+        public static void PrintArray(params int[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
