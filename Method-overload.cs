8b27f9ba26945b3c8cbb6939b629e07306f10009
Adelaide <88384474+allytyson16@users.noreply.github.com>
2022-02-19 16:30:58 +0000

    Add files via upload

---
+++ Method-overload.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Method_overload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Add(5, 10);
+            Add("hello", "world");
+            Add(3.0, 5.2);
+        }
+        public static void Add(int x, int y)
+        {
+            int answer = x + y;
+            Console.WriteLine(answer);
+        }
+        public static void Add(string x, string y)
+        {
+            string answer = x + " " + y;
+            Console.WriteLine(answer);
+        }
+        public static void Add(double x, double y)
+        {
+            double answer = x + y;
+            Console.WriteLine(answer);
+        }
+    }
+}
