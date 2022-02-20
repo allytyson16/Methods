685e83f495b256bcc0a3a14cf3e185ba5bf91351
Adelaide <88384474+allytyson16@users.noreply.github.com>
2022-02-20 07:58:08 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ooptional_parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Add(5);
+            Add(4, 5, 5);
+            Add(4, z: 5, y: 5);
+            Add(4, z: 5);
+        }
+        public static void Add(int x, int y = 1, int z = 2)
+        {
+            int answer = x + y + z;
+            Console.WriteLine(answer);
+        }
+    }
+}
